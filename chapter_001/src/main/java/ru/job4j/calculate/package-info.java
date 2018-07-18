3c58585c69ea30484ed2696487c99f25ec4073ec
Alexander <alexustmsk@gmail.com>
2018-07-18 10:28:51 +0300

    1.3.JavaDoc. Code convetion.

---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Alexander Ustyuzhanin (mailto:alexustmsk@gmail.com)
+ * @version $Id$
+ * @since 18.07.2018
+ */
+package main.java.ru.job4j.calculate;
