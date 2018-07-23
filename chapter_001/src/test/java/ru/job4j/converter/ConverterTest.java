be69172f8676a5b13587244dd8e18a8f9a390480
Alexander <alexustmsk@gmail.com>
2018-07-23 22:58:17 +0300

    3.2. Конвертер валюты.

---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConverterTest {
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+    @Test
+    public void when60DollarToRubleThen1() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(60);
+        assertThat(result, is(3600));
+    }
+    @Test
+    public void when70DollarToRubleThen1() {
+        Converter converter = new Converter();
+        int result = converter.euroToRuble(70);
+        assertThat(result, is(4900));
+    }
+}
