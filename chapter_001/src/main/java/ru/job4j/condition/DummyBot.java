3508e3ff312b299522c139f7ceba3a26756edc7b
Alexander <alexustmsk@gmail.com>
2018-07-25 22:09:35 +0300

    4.1. Глупый бот.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+/**
+ * @author Alexander Ustyuzhanin
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl;
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        } else {
+            rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        }
+        return rsl;
+    }
+}
