35b7530d92262ecac9fcc3c8a25c4a750858880b
Alexander <alexustmsk@gmail.com>
2018-10-27 23:59:41 +0300

    1. Реализация событий на внутренних классах.Correct

--- chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -179,12 +179,12 @@ public class MenuTracker {
         public void execute(Input input, Tracker tracker) {
             System.out.println("------------ Поиск заявки по Имени --------------");
             String name = input.ask("Введите Имя заявки: ");
-            if (name != null) {
-                System.out.println("Найдено кол-во заявок: " + tracker.findByName(name).length);
-                for (int i = 0; i < tracker.findByName(name).length; i++) {
-                    System.out.println(String.format("%s. Имя заявки: %s Описание: %s",
-                            i, tracker.findByName(name)[i].getName(), tracker.findByName(name)[i].getDescription()));
-                }
+                if (tracker.findByName(name).length != 0) {
+                    System.out.println("Найдено кол-во заявок: " + tracker.findByName(name).length);
+                    for (int i = 0; i < tracker.findByName(name).length; i++) {
+                        System.out.println(String.format("%s. Имя заявки: %s Описание: %s",
+                                i+1, tracker.findByName(name)[i].getName(), tracker.findByName(name)[i].getDescription()));
+                    }
             } else {
                 System.out.println("Заявка с таким Именем не найдена");
             }
