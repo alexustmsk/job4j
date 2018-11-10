bf9c01d451e88923c96bf31a8f01b3b3c6ec4475
Alexander <alexustmsk@gmail.com>
2018-11-10 23:00:26 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -2,18 +2,29 @@ package ru.job4j.tracker;
 
 import java.util.List;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Input input;
+
+    public ValidateInput(final Input input) {
+        this.input = input;
+    }
+
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
     public int ask(String question, List<Integer> range) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe) {
                 System.out.println("Please select key from menu.");
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data agein.");
+                System.out.println("Please enter validate data again.");
             }
         } while (invalid);
         return value;
